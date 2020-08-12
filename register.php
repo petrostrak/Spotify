7b0beb3f1afa9b3524f14101817456865a1d8fff
petros <pit.trak@gmail.com>
2020-08-12 22:19:05 +0300

    Separated the code to different files

--- register.php
+++ register.php
@@ -1,44 +1,7 @@
 <?php 
-
-function sanitizeFormUsername($inputText){
-    $inputText = strip_tags($inputText); // removes all html elements
-    $inputText = str_replace(" ", "", $inputText); // removes spacing
-    return $inputText;
-}
-
-function sanitizeFormString($inputText){
-    $inputText = sanitizeFormUsername($inputText);
-    $inputText = ucfirst(strtolower($inputText)); // first we convert the string value toLowerCase with strtoLower and then only the first letter to upperCase with ucfirst
-    return $inputText;
-}
-
-function sanitizeFormPassword($inputText){
-    $inputText = strip_tags($inputText);
-    return $inputText;
-}
-
-if (isset($_POST['loginButton'])) {
-    //echo "login button was pressed";
-}
-
-if (isset($_POST['registerButton'])) {
-    $username = sanitizeFormUsername($_POST['username']);
-
-    $firstName = sanitizeFormString($_POST['firstName']);
-
-    $lastName = sanitizeFormString($_POST['lastName']);
-
-    $email = sanitizeFormString($_POST['email']);
-
-    $email2 = sanitizeFormString($_POST['email2']);
-
-    $password = sanitizeFormPassword($_POST['password']);
-
-    $password2 = sanitizeFormPassword($_POST['password2']);
-}
-
+    include("includes/handlers/register-handler.php");
+    include("includes/handlers/login-handler.php");
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
