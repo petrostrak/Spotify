64f754a383b009491469d6eb91fa52bbedb9357a
petros <pit.trak@gmail.com>
2020-08-13 02:03:08 +0300

    Connected to MySQL from PHP

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php 
+    ob_start();
+
+    $timezone = date_default_timezone_set("Europe/Athens");
+
+    $con = mysqli_connect("127.0.0.1", "root", "root", "Spotify");
+    if(mysqli_connect_errno()) {
+        echo "Failed to connect : " .mysqli_connect_errno();
+    }
+?>
