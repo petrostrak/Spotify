12da526fb33fd4610a3e33a19e9af4f14005c558
petros <pit.trak@gmail.com>
2020-08-13 12:50:26 +0300

    fixed an error in query

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,94 +1,104 @@
-<?php 
-    class Account {
+<?php
+	class Account {
 
-        private $con;
-        private $errorArray;
+		private $con;
+		private $errorArray;
 
-        public function __construct($con) {
-            $this->con = $con;
-            $this->errorArray = array();
-        }
+		public function __construct($con) {
+			$this->con = $con;
+			$this->errorArray = array();
+		}
 
-        public function register($un, $fn, $ln, $em, $em2, $pw, $pw2) {
-            $this->validateUsername($un);
-            $this->validateFirstName($fn);
-            $this->validateLastName($ln);
-            $this->validateEmails($em, $em2);
-            $this->validatePasswords($pw, $pw2);
+		public function register($un, $fn, $ln, $em, $em2, $pw, $pw2) {
+			$this->validateUsername($un);
+			$this->validateFirstName($fn);
+			$this->validateLastName($ln);
+			$this->validateEmails($em, $em2);
+			$this->validatePasswords($pw, $pw2);
 
-            if(empty($this->errorArray) == true){
-                // Insert into db
-                return insertUserDetails($un, $fn, $ln, $em, $pw);
-            } else {
-                return false;
-            }
-        }
+			if(empty($this->errorArray) == true) {
+				//Insert into db
+				return $this->insertUserDetails($un, $fn, $ln, $em, $pw);
+			}
+			else {
+				return false;
+			}
 
-        private function insertUserDetails($un, $fn, $ln, $em, $pw) {
-            $encryptedPw = md5($pw);
-            $profilePic = "assets/images/profile-pics/head_emerald1.jpg";
-            $date = date("Y-m-d");
+		}
 
-            $result = mysqli_query($this->con, "INSERT INTO users VALUES('', '$un', '$fn', '$ln', '$em', '$encryptedPw', '$date', '$profilePic')");
-            return $result;
-        }
+		public function getError($error) {
+			if(!in_array($error, $this->errorArray)) {
+				$error = "";
+			}
+			return "<span class='errorMessage'>$error</span>";
+		}
 
-        public function getError($error) {
-            if(!in_array($error, $this->errorArray)) {
-                $error = "";
-            }
-            return "<span class='errorMessage'>$error</span>";
-        }
+		private function insertUserDetails($un, $fn, $ln, $em, $pw) {
+			$encryptedPw = md5($pw);
+			$profilePic = "assets/images/profile-pics/head_emerald.png";
+			$date = date("Y-m-d");
 
-        private function validateUsername($un){
-            if(strlen($un) > 25 || strlen($un) < 5){
-                array_push($this->errorArray, Constants::$usernameCharacters);
-                return;
-            }
+			$result = mysqli_query($this->con, "INSERT INTO users VALUES (NULL, '$un', '$fn', '$ln', '$em', '$encryptedPw', '$date', '$profilePic')");
 
-            //TODO: check if username exists
-        }
+			return $result;
+		}
 
-        private function validateFirstName($fn){
-            if(strlen($fn) > 25 || strlen($fn) < 2){
-                array_push($this->errorArray, Constants::$firstNameCharacters);
-                return;
-            }
-        }
+		private function validateUsername($un) {
 
-        private function validateLastName($ln){
-            if(strlen($ln) > 25 || strlen($ln) < 2){
-                array_push($this->errorArray, Constants::$lastNameCharacters);
-                return;
-            }
-        }
+			if(strlen($un) > 25 || strlen($un) < 5) {
+				array_push($this->errorArray, Constants::$usernameCharacters);
+				return;
+			}
 
-        private function validateEmails($em, $em2){
-            if($em != $em2){
-                array_push($this->errorArray, Constants::$emailsDoNotMatch);
-                return;
-            }
-            if(!filter_var($em, FILTER_VALIDATE_EMAIL)){
-                array_push($this->errorArray, Constants::$emailInvalid);
-                return;                
-            }
+			//TODO: check if username exists
 
-            //TODO: Check that email hasn't been used yet
-        }
+		}
 
-        private function validatePasswords($pw, $pw2){
-            if($pw != $pw2){
-                array_push($this->errorArray, Constants::$passwordsDoNotMatch);
-                return;                
-            }
-            if(preg_match('/[^A-Za-z0-9]/', $pw)){
-                array_push($this->errorArray, Constants::$passwordNotAlphanumeric);
-                return; 
-            }
-            if(strlen($pw) > 25 || strlen($pw) < 5){
-                array_push($this->errorArray, Constants::$passwordCharacters);
-                return;
-            }
-        }
-    }
+		private function validateFirstName($fn) {
+			if(strlen($fn) > 25 || strlen($fn) < 2) {
+				array_push($this->errorArray, Constants::$firstNameCharacters);
+				return;
+			}
+		}
+
+		private function validateLastName($ln) {
+			if(strlen($ln) > 25 || strlen($ln) < 2) {
+				array_push($this->errorArray, Constants::$lastNameCharacters);
+				return;
+			}
+		}
+
+		private function validateEmails($em, $em2) {
+			if($em != $em2) {
+				array_push($this->errorArray, Constants::$emailsDoNotMatch);
+				return;
+			}
+
+			if(!filter_var($em, FILTER_VALIDATE_EMAIL)) {
+				array_push($this->errorArray, Constants::$emailInvalid);
+				return;
+			}
+
+			//TODO: Check that username hasn't already been used
+
+		}
+
+		private function validatePasswords($pw, $pw2) {
+			
+			if($pw != $pw2) {
+				array_push($this->errorArray, Constants::$passwordsDoNotMatch);
+				return;
+			}
+
+			if(preg_match('/[^A-Za-z0-9]/', $pw)) {
+				array_push($this->errorArray, Constants::$passwordNotAlphanumeric);
+				return;
+			}
+
+			if(strlen($pw) > 30 || strlen($pw) < 5) {
+				array_push($this->errorArray, Constants::$passwordCharacters);
+				return;
+			}
+		}
+	}
 ?>
--- includes/handlers/register-handler.php
+++ includes/handlers/register-handler.php
@@ -17,20 +17,22 @@ function sanitizeFormPassword($inputText){
     return $inputText;
 }
 
-if (isset($_POST['registerButton'])) {
-    $username = sanitizeFormUsername($_POST['username']);
-    $firstName = sanitizeFormString($_POST['firstName']);
-    $lastName = sanitizeFormString($_POST['lastName']);
-    $email = sanitizeFormString($_POST['email']);
-    $email2 = sanitizeFormString($_POST['email2']);
-    $password = sanitizeFormPassword($_POST['password']);
-    $password2 = sanitizeFormPassword($_POST['password2']);
+if(isset($_POST['registerButton'])) {
+	//Register button was pressed
+	$username = sanitizeFormUsername($_POST['username']);
+	$firstName = sanitizeFormString($_POST['firstName']);
+	$lastName = sanitizeFormString($_POST['lastName']);
+	$email = sanitizeFormString($_POST['email']);
+	$email2 = sanitizeFormString($_POST['email2']);
+	$password = sanitizeFormPassword($_POST['password']);
+	$password2 = sanitizeFormPassword($_POST['password2']);
 
-    $wasSuccessfull = $account->register($username, $firstName, $lastName, $email2, $email, $password, $password2);
+	$wasSuccessful = $account->register($username, $firstName, $lastName, $email, $email2, $password, $password2);
+    
+	if($wasSuccessful == true) {
+		header("Location: index.php");
+	}
 
-    if($wasSuccessfull) {
-        header("Location: index.php");
-    }
 }
 
 ?>
