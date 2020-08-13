d0bb91fda169e4679fcc9182d03821b474c7dc51
petros <pit.trak@gmail.com>
2020-08-13 23:18:03 +0300

    added volume bar

--- index.php
+++ index.php
@@ -71,9 +71,18 @@ else {
                     </div>
                 </div>
             </div>
-            <div id="nowPlayingRight">
-
-            </div>
+			<div id="nowPlayingRight">
+				<div class="volumeBar">
+					<button class="controlButton volume" title="Volume button">
+						<img src="assets/images/icons/volume.png" alt="Volume">
+					</button>
+					<div class="progressBar">
+						<div class="progressBarBg">
+							<div class="progress"></div>
+						</div>
+					</div>
+				</div>
+			</div>
         </div>
     </div>
 </body>
