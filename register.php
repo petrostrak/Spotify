633478a46e0d3d8aa133ec4f21814f9c29d7f6cb
petros <pit.trak@gmail.com>
2020-08-13 19:33:37 +0300

    Added errorMessage and checkmark styles

--- register.php
+++ register.php
@@ -51,7 +51,7 @@ function getInputValue($name)
 					<p>
 						<?php echo $account->getError(Constants::$loginFailed) ?>
 						<label for="loginUsername">Username</label>
-						<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. petrosTrak" required>
+						<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. petrosTrak" value="<?php getInputValue('loginUsername') ?>" required>
 					</p>
 					<p>
 						<label for="loginPassword">Password</label>
