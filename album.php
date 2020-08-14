0d63ebba6c4664a1bf76470239380d21da19cc2b
petros <pit.trak@gmail.com>
2020-08-14 23:22:56 +0300

    Displaying the number of songs in the album

--- album.php
+++ album.php
@@ -15,7 +15,8 @@ $artist = $album->getArtist();
 	</div>
 	<div class="rightSection">
 		<h2><?php echo $album->getTitle(); ?></h2>
-		<span>By <?php echo $artist->getName(); ?></span>
+		<p>By <?php echo $artist->getName(); ?></p>
+		<p><?php echo $album->getNumberOfSongs(); ?> songs</p>
 	</div>
 </div>
 <?php include("includes/footer.php"); ?>
