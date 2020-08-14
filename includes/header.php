099bec7f07b6b467096cc414885939586709069a
petros <pit.trak@gmail.com>
2020-08-14 21:02:33 +0300

    Separated code

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<?php
+include("includes/config.php");
+
+//session_destroy(); LOGOUT
+
+if (isset($_SESSION['userLoggedIn'])) {
+    $userLoggedIn = $_SESSION['userLoggedIn'];
+} else {
+    header("Location: register.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <title>Welcome to Spotify</title>
+</head>
+
+<body>
+    <div id="mainContainer">
+        <div id="topContainer">
+            <?php include("includes/navBarContainer.php"); ?>
+            <div id="mainViewContainer">
+                <div id="mainContent">
--- index.php
+++ index.php
@@ -1,33 +1,3 @@
-<?php
-include("includes/config.php");
+<?php include("includes/header.php"); ?>
 
-//session_destroy(); LOGOUT
-
-if (isset($_SESSION['userLoggedIn'])) {
-    $userLoggedIn = $_SESSION['userLoggedIn'];
-} else {
-    header("Location: register.php");
-}
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-    <title>Welcome to Spotify</title>
-</head>
-
-<body>
-    <div id="mainContainer">
-        <div id="topContainer">
-            <?php include("includes/navBarContainer.php");?>
-        </div>
-        <?php include("includes/nowPlayingBar.php"); ?>
-    </div>
-</body>
-
-</html>
+<?php include("includes/footer.php"); ?> 
