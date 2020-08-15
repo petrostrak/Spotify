8b39ca180db8544e417a94c756ba8cfbe22b211a
petros <pit.trak@gmail.com>
2020-08-16 00:46:23 +0300

    Creating our initial playlist of 10 random songs

--- includes/header.php
+++ includes/header.php
@@ -21,6 +21,7 @@ if (isset($_SESSION['userLoggedIn'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    <script src="assets/js/script.js"></script>
     <title>Welcome to Spotify</title>
 </head>
 
