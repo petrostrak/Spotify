d73875f3556d631b456e5cc2a49b2fdeae3972e7
petros <pit.trak@gmail.com>
2020-08-17 19:33:48 +0300

    Styled playlist

--- yourMusic.php
+++ yourMusic.php
@@ -8,24 +8,23 @@
         <div class="buttonItems">
             <button class="button green" onclick="createPlaylist()">NEW PLAYLIST</button>
         </div>
+        <?php
+            $username = $userLoggedIn->getUsername();
+	        $playlistQuery = mysqli_query($con, "SELECT * FROM playlists WHERE owner='$username'");
+            if(mysqli_num_rows($playlistQuery) == 0) {
+                echo "<span class='noResults'>You don't have any playlists yet</span>";
+            }
+            while($row = mysqli_fetch_array($playlistQuery)) {
+                $playlist = new Playlist($con, $row);
+                echo "<div class='gridViewItem' role='link' tabindex='0' onClick='openPage(\"playlist.php?id=" . $playlist->getId() ."\")'>
+                            <div class='playlistImage'>
+                            <img src='assets/images/icons/playlist.png'>
+                        </div>
+                        <div class='gridViewInfo'>"
+                            . $playlist->getName() .
+                        "</div>
+			          </div>";
+	}
+?>
     </div>
 </div>
-
-<?php
-        $username = $userLoggedIn->getUsername();
-		$playlistQuery = mysqli_query($con, "SELECT * FROM playlists WHERE owner='$username'");
-        if(mysqli_num_rows($playlistQuery) == 0) {
-            echo "<span class='noResults'>You don't have any playlists yet</span>";
-        }
-        while($row = mysqli_fetch_array($playlistQuery)) {
-            $playlist = new Playlist($con, $row);
-            echo "<div class='gridViewItem'>
-                    <div class='playlistImage'>
-                        <img src='assets/images/icons/playlist.png'>
-                    </div>
-                    <div class='gridViewInfo'>"
-                    . $playlist->getName() .
-                    "</div>
-				</div>";
-		}
-	?>
