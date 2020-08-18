d9021464e8f3ba6b6a9f5f8b2ed48a30f5c2b98d
petros <pit.trak@gmail.com>
2020-08-18 03:14:21 +0300

    Fixed a bug where db wouldn't receive data

--- includes/handlers/ajax/addToPlaylist.php
+++ includes/handlers/ajax/addToPlaylist.php
@@ -2,12 +2,13 @@
 include("../../config.php");
 
 if(isset($_POST['playlistId']) && isset($_POST['songId'])) {
-	$playlistId = $_POST['playlistId'];
 	$songId = $_POST['songId'];
+	$playlistId = $_POST['playlistId'];
 	$orderIdQuery = mysqli_query($con, "SELECT MAX(playlistOrder) + 1 as playlistOrder FROM playlistSongs WHERE playlistId='$playlistId'");
 	$row = mysqli_fetch_array($orderIdQuery);
 	$order = $row['playlistOrder'];
-    $query = mysqli_query($con, "INSERT INTO `playlistSongs` VALUES( NULL, '$songId', '$playlistId', '$order')");
+	$query = mysqli_query($con, "INSERT INTO `playlistSongs` VALUES (NULL, '$songId', '$playlistId', '$order')");
+	
 }
 else {
 	echo "PlaylistId or songId was not passed into addToPlaylist.php";
