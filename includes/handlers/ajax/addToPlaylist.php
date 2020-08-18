429ba6a2ba27229cd79d18c62628c719359ebce1
petros <pit.trak@gmail.com>
2020-08-18 12:06:00 +0300

    Set settings page

--- includes/handlers/ajax/addToPlaylist.php
+++ includes/handlers/ajax/addToPlaylist.php
@@ -4,7 +4,7 @@ include("../../config.php");
 if(isset($_POST['playlistId']) && isset($_POST['songId'])) {
 	$songId = $_POST['songId'];
 	$playlistId = $_POST['playlistId'];
-	$orderIdQuery = mysqli_query($con, "SELECT MAX(playlistOrder) + 1 as playlistOrder FROM playlistSongs WHERE playlistId='$playlistId'");
+	$orderIdQuery = mysqli_query($con, "SELECT MAX(playlistOrder) + 1 as 'playlistOrder' FROM playlistSongs WHERE playlistId='$playlistId'");
 	$row = mysqli_fetch_array($orderIdQuery);
 	$order = $row['playlistOrder'];
 	$query = mysqli_query($con, "INSERT INTO `playlistSongs` VALUES (NULL, '$songId', '$playlistId', '$order')");
