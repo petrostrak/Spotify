f0885a77d33fb845d788d50fec6f2563c5b34dcb
petros <pit.trak@gmail.com>
2020-08-18 13:13:05 +0300

    Update password function

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,6 +53,16 @@ function updateEmail(emailClass) {
 		})
 }
 
+function updatePassword(oldPasswordClass, newPasswordClass1, newPasswordClass2) {
+	var oldPassword = $("." + oldPasswordClass).val()
+	var newPassword1 = $("." + newPasswordClass1).val()
+	var newPassword2 = $("." + newPasswordClass2).val()
+	$.post("includes/handlers/ajax/updatePassword.php", { oldPassword, newPassword1, newPassword2, username: userLoggedIn })
+		.done(function(response) {
+			$("." + oldPasswordClass).nextAll(".message").text(response)
+		})
+}
+
 function removeFromPlaylist(button, playlistId) {
 	var songId = $(button).prevAll(".songId").val()
 	
