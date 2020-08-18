44b6a24e22497d518fe828ccb5d1e1fb23629fa5
petros <pit.trak@gmail.com>
2020-08-18 13:42:36 +0300

    Changed save button to update

--- updateDetails.php
+++ updateDetails.php
@@ -7,7 +7,7 @@ include("includes/includedFiles.php");
         <h2>EMAIL</h2>
         <input type="text" class="email" name="email" placeholder="Email address..." value="<?php echo $userLoggedIn->getEmail(); ?>">
         <span class="message"></span>
-        <button class="button" onclick="updateEmail('email')">SAVE</button>
+        <button class="button" onclick="updateEmail('email')">UPDATE</button>
     </div>
     <div class="container">
         <h2>PASSWORD</h2>
@@ -15,6 +15,6 @@ include("includes/includedFiles.php");
         <input type="password" class="newPassword1" name="newPassword1" placeholder="New password">
         <input type="password" class="newPassword2" name="newPassword2" placeholder="Confirm password">
         <span class="message"></span>
-        <button class="button" onclick="updatePassword('oldPassword', 'newPassword1', 'newPassword2')">SAVE</button>
+        <button class="button" onclick="updatePassword('oldPassword', 'newPassword1', 'newPassword2')">UPDATE</button>
     </div>
 </div>
